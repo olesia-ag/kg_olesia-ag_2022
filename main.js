be014ee5e8da83355deaac6c406e106eeeb26c39
Olesia Guidi <olgarbuzova@gmail.com>
2021-05-03 12:41:01 -0400

    added test for the single digit case and refactored function to pass it

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
-
+function transformNumber(num) {
+	let numStr = num.toString();
+	switch (numStr) {
+		case '0':
+			return 'Zero';
+		case '1':
+			return 'One';
+		case '2':
+			return 'Two';
+		case '3':
+			return 'Three';
+		case '4':
+			return 'Four';
+		case '5':
+			return 'Five';
+		case '6':
+			return 'Six';
+		case '7':
+			return 'Seven';
+		case '8':
+			return 'Eight';
+		case '9':
+			return 'Nine';
+		default:
+			console.warn('ivalid argument!');
+	}
+}
 
 export function convertArray(arr) {
-  return
+	if (!arr.length) return arr;
+	else {
+		return arr.map((elem) => {
+			return transformNumber(elem);
+		});
+	}
 }
--- test/convertArray.spec.js
+++ test/convertArray.spec.js
@@ -1,2 +1,21 @@
 import { expect } from 'chai';
 import { convertArray } from '../main.js';
+
+
+describe('convertArray function which accepts an array of integers and returns an array of strings representing the phonetic equivalent of the integer', () => {
+	it('returns empty array if the argument is an empty array', (done) => {
+		expect(convertArray([])).to.eql([]);
+		done();
+	});
+  it('returns correct result if an argument array includes single digit integeres', (done) => {
+		expect(convertArray([2])).to.eql(['Two']);
+		expect(convertArray([3, 5, 7, 4])).to.eql([
+			'Three',
+			'Five',
+			'Seven',
+			'Four',
+		]);
+		done();
+	});
+
+})
